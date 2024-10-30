ea86ed179aa15e88d3430e9e9f4116d2ae797b67
Nemanja Stankovic <nemanjas0823@hotmail.com>
2024-10-30 01:22:19 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(){
+return `This Saturday, I want to roller-skate!`
+}
+function saturdayFun(someStr= "roller-skate"){
+    return `This Saturday, I want to ${someStr}!`
+}
+function mondayWork(){
+   return `This Monday, I will go to the office.` 
+}
+function mondayWork(someStr= "go to the office"){
+    return `This Monday, I will ${someStr}.`
+}
+function wrapAdjective(wrapper){
+return function (someStr){
+    return `You are ${wrapper}${someStr}${wrapper}!`
+}
+}
